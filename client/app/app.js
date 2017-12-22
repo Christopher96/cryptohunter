c18697152def792bb80156d150cff8463091602f
Christopher Gauffin <christopher.gauffin@gmail.com>
2017-12-22 16:52:08 +0100

    controllers

---
+++ client/app/app.js
@@ -0,0 +1,1 @@
+angular.module('cryptohunter', ['appRoutes', 'userControllers']);
---
+++ client/app/controllers/userCtrl.js
@@ -0,0 +1,7 @@
+angular.module('userControllers', [])
+.config(function() {
+    console.log("hello");
+})
+    .controller('signUpCtrl', function(){
+        console.log('signup');
+    })
